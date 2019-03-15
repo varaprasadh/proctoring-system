fef0c32bb31b6bd358a34ce36605b1f57a1dff5c
varaprasadh <varaprasadh.a@gmail.com>
2019-03-15 21:15:08 +0530

    modifyhadler

--- rotes/modifyHandler.js
+++ rotes/modifyHandler.js
@@ -5,7 +5,18 @@ const connection = require("../dbconnection").connection;
 
 Router.get('/modify/:value',(req,res)=>{
     var value=req.params.value;
-    
+    new Promise((resolve,reject)=>{
+        var sql=`select name,regdNo,department from faculty where name like '%${value}% or regdNo like '%${value}%'`;
+        connection.query(sql,(err,result)=>{
+            if(err){
+                console.log(err.sqlMessage);
+                reject(err.sqlMessage);
+            }
+            console.log(result);
+            resolve(result);
+            
+        })
+    })
 })
 
 
